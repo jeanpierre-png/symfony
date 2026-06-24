2a5af400271f020804f9d9369f799b9a75a23227
NOZA Jean-Pierre <nozajean-pierre@macbook-air-de-jean-pierre-2.home>
2026-06-24 16:06:33 -0300

    Ajout de divers fonctionnalités

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -54,7 +54,12 @@ class RegistrationController extends AbstractController
 
             // do anything else you need here, like send an email
 
-            return $security->login($user, LoginFormAuthenticator::class, 'main');
+            $this->addFlash(
+                'success',
+                'un email de confirmation vous a été envoyé.'
+            );
+
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
@@ -81,6 +86,6 @@ class RegistrationController extends AbstractController
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_login');
     }
 }
--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -5,17 +5,35 @@ namespace App\Controller;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ShopController extends AbstractController
 {
     #[Route('/shop', name: 'app_shop')]
-    public function index(ProductRepository $productRepository): Response
+    public function index(ProductRepository $productRepository, Request $request): Response
     {
-        $products = $productRepository->findAll();
+        $min = $request->query->get('min');
+        $max = $request->query->get('max');
 
+        if ($min !== null && $max !== null) {
+            $products = $productRepository->createQueryBuilder('p')
+            ->where('p.price >= :min')
+            ->andWhere('p.price <= :max')
+            ->setParameter('min', $min)
+            ->setParameter('max', $max)
+            ->getQuery()
+            ->getResult();
+
+        } else {
+            $products = $productRepository->findAll();
+        }
+        
         return $this->render('shop/index.html.twig', [
             'products' => $products,
+            'min' => $min,
+            'max' => $max,
         ]);
+
     }
 }
